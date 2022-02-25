068c36ca8e34dce1bb8b0137b8bc673d049139d3
Harsh Tiwari <harsh29tiwarig@gmail.com>
2022-02-25 14:56:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var n= new Date();
+var y=n.getFullYear();
+var m=n.getMonth()+1;
+var d=n.getDate();
+document.getElementById('date').innerHTML=d+'/'+m +'/'+y;
+// delete
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// checked sign
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// add
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input_box").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("ul").appendChild(li);
+  }
+  document.getElementById("input_box").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
